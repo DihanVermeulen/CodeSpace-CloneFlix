6cf0b5fe2d070bf0261f8fd4b50f833b30a62a7d
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-09-16 15:07:17 +0200

    Changed boilerplate app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import shortened_logo from './shortened_logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={shortened_logo} className="app_logo" alt="logo" />
+        </div>
       </header>
     </div>
   );
