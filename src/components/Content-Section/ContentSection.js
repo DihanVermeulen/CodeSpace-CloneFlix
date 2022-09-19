d2fbfecb7bcc345c20f31bf5060d4125f4b028bd
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-09-19 09:43:49 +0200

    updated

--- src/components/Content-Section/ContentSection.js
+++ src/components/Content-Section/ContentSection.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import './ContentSection.css';
 
 const ContentSection = () => {
   return (
-    <div>ContentSection</div>
+    <div className='content-section'>
+
+    </div>
   )
 }
 
