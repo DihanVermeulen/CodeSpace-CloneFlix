24b715113755a8e2f52b45b92082e586d8a354a4
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-09-23 13:21:57 +0200

    Removed boolean as it is not needed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,16 @@
 import './Header.css';
 import shortened_logo from '../../assets/images/shortened_logo.svg';
 import longer_logo from '../../assets/images/longer_logo.svg';
-import profile1 from '../../assets/images/profile1.png';
+import profile_photo from '../../assets/images/profile_photo.png';
 import { useEffect, useState } from 'react';
 
 const Header = () => {
     let logo = shortened_logo;
-    const [isMobile, setIsMobile] = useState(false);
 
     const handleResize = () => {
         if (window.innerWidth <= 600) {
-            setIsMobile(true)
             document.querySelector('.main-header--top_logo').setAttribute('src', shortened_logo);
         } else {
-            setIsMobile(false)
             document.querySelector('.main-header--top_logo').setAttribute('src', longer_logo);
         }
     }
@@ -33,8 +30,7 @@ const Header = () => {
         <header className='main-header'>
             <section className='main-header--top'>
                 <img className='main-header--top_logo' src={logo} alt='logo'></img>
-
-                <img className='main-header--top_profile' src={profile1} alt='profile'></img>
+                <img className='main-header--top_profile' src={profile_photo} alt='profile'></img>
             </section>
             <section className='main-header--bottom'>
                 <div className='main-header--bottom_content'>
@@ -42,6 +38,9 @@ const Header = () => {
                     <div>
                         <button className='cloneflix-button_primary'>See trailer</button>
                         <button className='cloneflix-button_secondary'>+ Watch List</button>
+                    </div>
+                    <div>
+                        <button className='cloneflix-button_tertiary'>My List</button>
                     </div>
                 </div>
             </section>
