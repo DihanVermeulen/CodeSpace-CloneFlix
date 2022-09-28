fae25454584a1cef2f8436e357005168ac210980
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-09-28 11:48:53 +0200

    Added request to other api

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,6 @@
 const requests = {
     fetchAllMovies: 'https://project-apis.codespace.co.za/api/movies',
+    fetchSchalkMovies: 'https://netflix-mock-codespace.netlify.app/api.json',
 }
 
 export default requests;
