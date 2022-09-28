aa45c0388e931c6a09ecd54e360a4ccfb2dfb244
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-09-28 11:47:18 +0200

    put fetch request into content section

--- src/components/Content-Section/ContentSection.js
+++ src/components/Content-Section/ContentSection.js
@@ -11,40 +11,23 @@ const ContentSection = () => {
     async function fetchData() {
       const request = await fetch(requests.fetchAllMovies);
       const { data } = await request.json();
-
-      switch (props.value) {
-        case 'coming_soon':
-          let comingSoonMovies = data.filter(movie => {
-            return movie.is_coming_soon === 1
-          })
-          setMovies(comingSoonMovies);
-          break;
-        case 'top_rated':
-          let topRatedMovies = data.filter(movie => {
-            return movie.rating >= 5
-          })
-          setMovies(topRatedMovies);
-          break;
-        default:
-          let availableMovies = data.filter(movie => {
-            return movie.is_coming_soon !== 1
-          })
-          setMovies(availableMovies);
-          break;
-      }
+      setMovies(data);
 
       return request
     }
     fetchData();
   }, []);
 
-  return (
-    <div className='content-section'>
-      <Row title='Coming Soon' value='coming_soon' movies={movies} />
-      <Row title='Top Rated' value='top_rated' movies={movies} />
-      <Row title='All movies' value='all_movies' movies={movies} />
+  if (!movies) {
+    return <div>loading...</div>
+  }
+  else {
+    return <div className='content-section'>
+      {movies && <Row title='Coming Soon' value='coming_soon' movies={movies} />}
+      {movies && <Row title='Top Rated' value='top_rated' movies={movies} />}
+      {movies && <Row title='All movies' value='all_movies' movies={movies} />}
     </div>
-  )
+  }
 }
 
 
