1717bc5d030b9f46268cbbd213e3e2544ded58f3
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-09-30 10:10:36 +0200

    Removed isSignedIn as it is not needed at this moment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Header from './components/header/Header';
 import ContentSection from './components/Content-Section/ContentSection';
-import isSignedIn from './components/runtime';
 import './App.css';
-
-console.log(isSignedIn)
 
 function App() {
   return (
