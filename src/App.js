db5f2e8128bbd85131491f195b469b6c3fcbf47f
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-04 12:26:56 +0200

    Updated routes to work with new version of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Header from './components/header/Header';
-import ContentSection from './components/Content-Section/ContentSection';
 import './App.css';
 import './utils/helpers.css';
+import { Home } from './pages/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login/Login';
+import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ContentSection />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
