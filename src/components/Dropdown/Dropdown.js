9988508c73d807bd68bdd4e966f23e734b14e74f
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-06 15:24:38 +0200

    Now when clicked outside of dropdown it will close

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Dropdown.css';
 import isSignedIn from '../../utils/utils'
 
