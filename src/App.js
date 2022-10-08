5d723bffa41a5928049ae6038ecb9295411a2e11
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-08 16:33:05 +0200

    Added function to see if there are movies in the watch list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import './utils/helpers.css';
 import { Home } from './pages/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login/Login';
-import requests from './requests';
+import checkIfWatchlistExists from './runtime';
 
 function App() {
+
+  useEffect(() => {
+    checkIfWatchlistExists();
+  })
+
   return (
     <Router>
       <div className="App">
