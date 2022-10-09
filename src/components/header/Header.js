8ba9a50e6f679c7f7ba5d0644d07cb2d8ffcb048
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-09 10:58:46 +0200

    Removed console.logs and removed unnecessary imports

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react';
 import { Dropdown } from '../Dropdown/Dropdown';
 import { DropdownButton } from '../Dropdown/DropdownButton/DropdownButton';
 import WatchList from '../WatchList/WatchList';
-import { Drawer } from '../WatchList/WatchList';
 import requests from '../../requests';
 
 const Header = () => {
@@ -29,21 +28,17 @@ const Header = () => {
             let movies = data.data;
             // FILERS MOVIES TO GET TOP RATED MOVIES
             let topRatedMovies = movies.filter(movie => {
-                console.log('filtering movies...')
                 return movie.rating >= 7
             })
 
             // GETS LENGTH OF ALL TOP RATED MOVIES
             let topRatedMoviesLength = Object.keys(topRatedMovies).length - 1;
-            console.log('Length of all movies', topRatedMoviesLength);
 
             // SETS A RANDOM NUMBER BETWEEN 1 AND THE AMOUNT OF TOP RATED MOVIES
             let randomNum = Math.floor(Math.random() * topRatedMoviesLength);
-            console.log('random number', randomNum);
 
             // SETS movieHeader EQUAL TO THE INDEX PROVIDED BY randomNum
             let selectedMovie = topRatedMovies[randomNum];
-            console.log('Selected Movie: ', selectedMovie);
             setMovieHeader(selectedMovie);
         }
 
@@ -58,17 +53,31 @@ const Header = () => {
             );
         }
         fetchData();
-
-        // setMovieCover("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg");
     }, []);
 
+    const addMovieToWatchList = (e) => {
+        let allMovies = movies.data;
+        let movieId = e.target.parentElement.id;
+        let selectedMovie;
+        allMovies.forEach(movie => {
+            if (movie.id == movieId) {
+                selectedMovie = movie;
+            }
+        });
+        let watchlistMovies = localStorage.getItem('watchlistMovies');
+        let parsedWatchlistMovies = JSON.parse(watchlistMovies);
+        parsedWatchlistMovies.push(selectedMovie);
+        localStorage.setItem('watchlistMovies', JSON.stringify(parsedWatchlistMovies));
+    }
+
+    // CHANGES LOGO ON WINDOW SIZE
     if (window.innerWidth <= 600) {
         logo = shortened_logo;
     }
     else {
         logo = longer_logo
     }
-    // "linear-gradient(to bottom, rgba(255,255,255,0) 50%, rgba(0,0,0,1) 100%)," + "url(" + { movieCover } + ")"
+
     return (
         <header className='main-header' style={{ backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0) 50%, rgba(0,0,0,1) 100%), url(${movieHeader.image})` }}>
             <section className='main-header--top'>
@@ -87,10 +96,9 @@ const Header = () => {
                     <h2 className='main-header--cover_title'>{movieHeader.name}</h2>
                     <div id={movieHeader.id}>
                         <button className='cloneflix-button_primary'>See trailer</button>
-                        <button className='cloneflix-button_secondary'>+ Watch List</button>
+                        <button onClick={addMovieToWatchList} className='cloneflix-button_secondary'>+ Watch List</button>
                     </div>
                     <div>
-                        {/* <button className='cloneflix-button_tertiary'>My List</button> */}
                         <WatchList class='cloneflix-button_tertiary' />
                     </div>
                 </div>
