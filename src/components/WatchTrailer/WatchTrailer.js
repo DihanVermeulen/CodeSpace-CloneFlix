2cda0ed58a7ef875252e63d9cccde22426c0b1e4
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-09 22:46:14 +0200

    Added component for watching trailer

---
+++ src/components/WatchTrailer/WatchTrailer.js
@@ -0,0 +1,19 @@
+import './WatchTrailer.css';
+export const WatchTrailer = () => {
+
+    const closeTrailer = () => {
+        document.querySelector('#watchtrailer iframe').contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+        document.querySelector('#watchtrailer').style.display = 'none';
+    }
+
+    return (
+        <article id='watchtrailer'>
+            <div onClick={closeTrailer} className='watchtrailer-close_button'>X</div>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1"
+                title="YouTube video player" frameborder="0" allow="autoplay"
+                allowfullscreen
+            >
+            </iframe>
+        </article>
+    )
+}
