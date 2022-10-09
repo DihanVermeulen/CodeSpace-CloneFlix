1289cd8b1fa5da70d28d2565fb3691a17b8a01b4
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-09 13:48:49 +0200

    Added function to get current logged in user

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,10 @@
-const isSignedIn = () => {
+export const isSignedIn = () => {
     console.log(localStorage.getItem('SignedIn'));
     let localStorageBool = localStorage.getItem('SignedIn');
     let bool = localStorageBool === 'true' ? true : false;
     return bool;
 }
 
-export default isSignedIn
+export const getCurrentLoggedInUser = () => {
+    return JSON.parse(localStorage.getItem('loggedInAs'));
+}
