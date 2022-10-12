f4a5166a6f0cc09d34af7c014a41f1c51737e3a2
Dihan Vermeulen <dihan.vermeulen12@gmail.com>
2022-10-12 08:29:54 +0200

    Updated requests

--- src/requests.js
+++
@@ -1,6 +0,0 @@
-const requests = {
-    fetchAllMovies: 'https://project-apis.codespace.co.za/api/movies',
-    fetchSchalkMovies: 'https://netflix-cs-api.netlify.app/',
-}
-
-export default requests;
